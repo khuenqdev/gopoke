bba11d74bb87f2c8cc2216123bc631ec7caa5b31
Brian Knight <brianknight144@gmail.com>
2023-05-23 22:31:28 +0700

    Add interfaces for berries and contests

--- pokeapi/berries.go
+++ pokeapi/berries.go
@@ -1,46 +1,83 @@
 package pokeapi
 
 type Berry struct {
-    ID               int       `json:"id,omitempty"`
-    Name             string    `json:"name,omitempty"`
-    GrowthTime       int       `json:"growth_time,omitempty"`
-    MaxHarvest       int       `json:"max_harvest,omitempty"`
-    NaturalGiftPower int       `json:"natural_gift_power,omitempty"`
-    Size             int       `json:"size,omitempty"`
-    Smoothness       int       `json:"smoothness,omitempty"`
-    SoilDryness      int       `json:"soil_dryness,omitempty"`
-    Firmness         *Resource `json:"firmness,omitempty"`
-    Flavors          []Flavor  `json:"flavors,omitempty"`
-    Item             *Resource `json:"item,omitempty"`
-    NaturalGiftType  *Resource `json:"natural_gift_type,omitempty"`
+	ID               int               `json:"id,omitempty"`
+	Name             string            `json:"name,omitempty"`
+	GrowthTime       int               `json:"growth_time,omitempty"`
+	MaxHarvest       int               `json:"max_harvest,omitempty"`
+	NaturalGiftPower int               `json:"natural_gift_power,omitempty"`
+	Size             int               `json:"size,omitempty"`
+	Smoothness       int               `json:"smoothness,omitempty"`
+	SoilDryness      int               `json:"soil_dryness,omitempty"`
+	Firmness         *NamedAPIResource `json:"firmness,omitempty"`
+	Flavors          []Flavor          `json:"flavors,omitempty"`
+	Item             *NamedAPIResource `json:"item,omitempty"`
+	NaturalGiftType  *NamedAPIResource `json:"natural_gift_type,omitempty"`
 }
 
 type Flavor struct {
-    Potency int       `json:"potency,omitempty"`
-    Flavor  *Resource `json:"flavor,omitempty"`
+	Potency int               `json:"potency,omitempty"`
+	Flavor  *NamedAPIResource `json:"flavor,omitempty"`
 }
 
 type BerryFirmness struct {
-    ID      int        `json:"id,omitempty"`
-    Name    string     `json:"name,omitempty"`
-    Berries []Resource `json:"berries,omitempty"`
-    Names   []Name     `json:"names,omitempty"`
+	ID      int                `json:"id,omitempty"`
+	Name    string             `json:"name,omitempty"`
+	Berries []NamedAPIResource `json:"berries,omitempty"`
+	Names   []Name             `json:"names,omitempty"`
 }
 
 type Name struct {
-    Name     string    `json:"name,omitempty"`
-    Language *Resource `json:"language,omitempty"`
+	Name     string            `json:"name,omitempty"`
+	Language *NamedAPIResource `json:"language,omitempty"`
 }
 
 type BerryFlavor struct {
-    ID          int              `json:"id,omitempty"`
-    Name        string           `json:"name,omitempty"`
-    Berries     []FlavorBerryMap `json:"berries,omitempty"`
-    ContestType *Resource        `json:"contest_type,omitempty"`
-    Names       []Name           `json:"names,omitempty"`
+	ID          int               `json:"id,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	Berries     []FlavorBerryMap  `json:"berries,omitempty"`
+	ContestType *NamedAPIResource `json:"contest_type,omitempty"`
+	Names       []Name            `json:"names,omitempty"`
 }
 
 type FlavorBerryMap struct {
-    Potency int       `json:"potency,omitempty"`
-    Berry   *Resource `json:"berry,omitempty"`
+	Potency int               `json:"potency,omitempty"`
+	Berry   *NamedAPIResource `json:"berry,omitempty"`
+}
+
+type BerryApiInterface interface {
+	ListBerries() (berryList *ResourceList, err error)
+	GetBerry(idOrName string) (berry *Berry, err error)
+	ListBerryFirmnesses() (berryFirmnessList *ResourceList, err error)
+	GetBerryFirmness(idOrName string) (berryFirmness *BerryFirmness, err error)
+	ListBerryFlavor() (berryFlavorList *ResourceList, err error)
+	GetBerryFlavor(idOrName string) (berryFlavor *BerryFlavor, err error)
+}
+
+type BerryApi struct {
+	client *PokeApi
+}
+
+func (s *BerryApi) ListBerries() (berryList *ResourceList, err error) {
+	return berryList, err
+}
+
+func (s *BerryApi) GetBerry(idOrName string) (berry *Berry, err error) {
+	return berry, err
+}
+
+func (s *BerryApi) ListBerryFirmnesses() (berryFirmnessList *ResourceList, err error) {
+	return berryFirmnessList, err
+}
+
+func (s *BerryApi) GetBerryFirmness(idOrName string) (berryFirmness *BerryFirmness, err error) {
+	return berryFirmness, err
+}
+
+func (s *BerryApi) ListBerryFlavor() (berryFlavorList *ResourceList, err error) {
+	return berryFlavorList, err
+}
+
+func (s *BerryApi) GetBerryFlavor(idOrName string) (berryFlavor *BerryFlavor, err error) {
+	return berryFlavor, err
 }
--- pokeapi/constests.go
+++ pokeapi/constests.go
@@ -1,1 +1,76 @@
 package pokeapi
+
+type ContestType struct {
+	ID          int               `json:"id,omitempty"`
+	Name        string            `json:"name,omitempty"`
+	BerryFlavor *NamedAPIResource `json:"berry_flavor,omitempty"`
+	Names       []ContestName     `json:"names,omitempty"`
+}
+
+type ContestName struct {
+	Name     string            `json:"name,omitempty"`
+	Color    string            `json:"color,omitempty"`
+	Language *NamedAPIResource `json:"language,omitempty"`
+}
+
+type ContestEffect struct {
+	ID                int          `json:"id,omitempty"`
+	Appeal            int          `json:"appeal,omitempty"`
+	Jam               int          `json:"jam,omitempty"`
+	EffectEntries     []Effect     `json:"effect_entries,omitempty"`
+	FlavorTextEntries []FlavorText `json:"flavor_text_entries,omitempty"`
+}
+
+type Effect struct {
+	Effect   string            `json:"effect,omitempty"`
+	Language *NamedAPIResource `json:"language,omitempty"`
+}
+
+type FlavorText struct {
+	FlavorText string            `json:"flavor_text,omitempty"`
+	Language   *NamedAPIResource `json:"language,omitempty"`
+}
+
+type SuperContestEffect struct {
+	ID                int                `json:"id,omitempty"`
+	Appeal            int                `json:"appeal,omitempty"`
+	FlavorTextEntries []FlavorText       `json:"flavor_text_entries,omitempty"`
+	Moves             []NamedAPIResource `json:"moves,omitempty"`
+}
+
+type ContestApiInterface interface {
+	ListContestTypes() (contestTypeList *ResourceList, err error)
+	GetContestType(idOrName string) (contestType *ContestType, err error)
+	ListContestEffects() (contestEffectList *ResourceList, err error)
+	GetContestEffect(id int) (contestEffect *ContestEffect, err error)
+	ListSuperContestEffects() (superContestEffectList *ResourceList, err error)
+	GetSuperContestEffect(id int) (superContestEffect *SuperContestEffect, err error)
+}
+
+type ContestApi struct {
+	client *PokeApi
+}
+
+func (s *ContestApi) ListContestTypes() (contestTypeList *ResourceList, err error) {
+	return contestTypeList, err
+}
+
+func (s *ContestApi) GetContestType(idOrName string) (contestType *ContestType, err error) {
+	return contestType, err
+}
+
+func (s *ContestApi) ListContestEffects() (contestEffectList *ResourceList, err error) {
+	return contestEffectList, err
+}
+
+func (s *ContestApi) GetContestEffect(id int) (contestEffect *ContestEffect, err error) {
+	return contestEffect, err
+}
+
+func (s *ContestApi) ListSuperContestEffects() (superContestEffectList *ResourceList, err error) {
+	return superContestEffectList, err
+}
+
+func (s *ContestApi) GetSuperContestEffect(id int) (superContestEffect *SuperContestEffect, err error) {
+	return superContestEffect, err
+}
